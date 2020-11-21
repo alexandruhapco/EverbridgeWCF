a5694ffca6d9660fc76f08124990f562d627fcc1
qwerty qwerty <hiichaki350@gmail.com>
2020-11-21 01:43:22 +0000

    compiling client side!

---
+++ EverbridgeClientSide/Program.cs
@@ -0,0 +1,21 @@
+﻿using EverbridgeClientSide.DoorServiceNamespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverbridgeClientSide {
+    class Program {
+        static void Main(string[] args) {
+            using (var client = new DoorServiceClient()) {
+                client.getAllDoors().ToList().ForEach(x => Console.WriteLine(x.ToString()));
+
+                while(true) {
+
+                }
+           
+            }
+        }
+    }
+}
